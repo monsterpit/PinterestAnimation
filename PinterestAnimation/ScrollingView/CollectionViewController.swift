c024d2b753e7b2be8a9045f361ca93dcf9ce2fb2
vikas <vikas.salian@myglamm.com>
2020-06-01 15:13:04 +0530

    working on it

---
+++ PinterestAnimation/ScrollingView/CollectionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CollectionViewController.swift
+//  PinterestAnimation
+//
+//  Created by MyGlamm on 6/1/20.
+//  Copyright © 2020 MyGlamm. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        
+    }
+
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+
+}
